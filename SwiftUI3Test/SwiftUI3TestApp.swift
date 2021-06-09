d67773d6732697d0146e4f583b00707a36a8f70f
David Fekke <david@fekke.com>
2021-06-09 11:49:50 -0400

    Initial Commit

---
+++ SwiftUI3Test/SwiftUI3TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3TestApp.swift
+//  SwiftUI3Test
+//
+//  Created by David Fekke on 6/9/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
